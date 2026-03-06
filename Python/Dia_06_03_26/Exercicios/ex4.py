e8931fa5b80d7993ddf1295ca1cbb7f1f5f4339a
Ricardo Augusto <tatianejc11@gmail.com>
2026-03-06 20:24:19 -0300

    Só confirmação

--- Python/Dia_06_03_26/Exercicios/ex4.py
+++ Python/Dia_06_03_26/Exercicios/ex4.py
@@ -4,11 +4,11 @@ salario = int(input("Adicione o valor de seu salário: "))
 
 if (salario > 1250):
   aumento = 0.10
-  salario_final = salario * aumento
+  salario_final = salario + (salario * aumento)
   print(f"Seu salário com aumento é de aproximadamente {salario_final}")
 elif (salario <= 1250):
   aumento = 0.15
-  salario_final = salario * aumento
+  salario_final = salario + (salario * aumento)
   print(f"Seu salário com aumento é de aproximadamente {salario_final}")
 else:
-  print("")
+  print("Você não inseriu números.")
