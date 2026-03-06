ba9e2787ad238675edb26ef3da110d751a49c915
Ricardo Augusto <tatianejc11@gmail.com>
2026-03-06 19:34:11 -0300

    Organização das pastas e inclusão dos exercícios propostos em sala de aula.

---
+++ Python/Dia_06_03_26/teste.py
@@ -0,0 +1,15 @@
+
+categoria = int(input("Digite uma categoria de 1 a 5: "))
+
+if (categoria == 1):
+  print("O preço da categoria é de R$10,00 reais")
+elif (categoria == 2):
+  print("O preço da categoria é de R$18,00 reais")
+elif (categoria == 3):
+  print("O preço da categoria é de R$23,00 reais")
+elif (categoria == 4):
+  print("O preço da categoria é de R$26,00 reias")
+elif (categoria == 5):
+  print("O preço da categoria é de R$31,00 reais")
+else:
+  print("Número não faz parte das categorias")
